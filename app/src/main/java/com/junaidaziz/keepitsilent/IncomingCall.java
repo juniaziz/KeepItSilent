03ff0f2a83703a741197840dd9a7bd50bcb4f73f
juniaziz <20404875+juniaziz@users.noreply.github.com>
2017-11-15 15:50:03 +0400

    toggleSound Functionality Working

--- app/src/main/java/com/junaidaziz/keepitsilent/IncomingCall.java
+++ app/src/main/java/com/junaidaziz/keepitsilent/IncomingCall.java
@@ -1,7 +1,9 @@
 package com.junaidaziz.keepitsilent;
 
+import android.app.NotificationManager;
 import android.content.*;
 import android.media.AudioManager;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -13,10 +15,12 @@ import android.widget.Toast;
 public class IncomingCall extends android.content.BroadcastReceiver {
 
     Context context;
+    static AudioManager audio;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         this.context = context;
+        audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
         Log.d("BroadCast: ", " Recieved");
 
@@ -26,28 +30,87 @@ public class IncomingCall extends android.content.BroadcastReceiver {
 //        context.startService(background);
     }
 
+    public void handleAudioManager(){
 
-    public void toggleSoundMode(){
-        Log.d("Permission: ", "Happy Jingles");
-
-        AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
         int currentMode = audio.getRingerMode();
 
-        switch (currentMode){
+        Log.d("BroadCast: ", "currentmode " + currentMode);
+        Log.d("BroadCast: ", "case 1: " + AudioManager.RINGER_MODE_NORMAL);
+        Log.d("BroadCast: ", "case 2: " + AudioManager.RINGER_MODE_VIBRATE);
+
+        switch (currentMode) {
             case AudioManager.RINGER_MODE_NORMAL:
                 audio.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Log.d("BroadCast: ", "case: RINGER MODE NORMAL");
                 Toast.makeText(context, "Viration Activated", Toast.LENGTH_LONG).show();
                 break;
             case AudioManager.RINGER_MODE_VIBRATE:
+                Log.d("BroadCast: ", "case; RINGER MODE VIBRATE");
                 audio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-                audio.setStreamVolume(AudioManager.STREAM_RING, audio.getStreamMaxVolume(AudioManager.STREAM_RING), 0);
-                audio.setStreamVolume(AudioManager.STREAM_NOTIFICATION, audio.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION), 0);
-                audio.setStreamVolume(AudioManager.STREAM_SYSTEM, audio.getStreamMaxVolume(AudioManager.STREAM_SYSTEM), 0);
-                audio.setStreamVolume(AudioManager.STREAM_MUSIC, audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
-
+                handleRingVolume();
                 Toast.makeText(context, "Normal mode Activated", Toast.LENGTH_LONG).show();
                 break;
+        }
+
+    }
+
+    public void handleRingVolume(){
+        audio.setStreamVolume(AudioManager.STREAM_RING, audio.getStreamMaxVolume(AudioManager.STREAM_RING), 0);
+        audio.setStreamVolume(AudioManager.STREAM_NOTIFICATION, audio.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION), 0);
+        audio.setStreamVolume(AudioManager.STREAM_SYSTEM, audio.getStreamMaxVolume(AudioManager.STREAM_SYSTEM), 0);
+        audio.setStreamVolume(AudioManager.STREAM_MUSIC, audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+        audio.setStreamVolume(AudioManager.STREAM_ALARM, audio.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+        Toast.makeText(context, "Max volume", Toast.LENGTH_LONG).show();
+    }
+
+
+    public void toggleSoundMode(){
+        Log.d("BroadCast: ", "toggleSoundMode");
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            handleAudioManager();
+        }
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            int currentMode = notificationManager.getCurrentInterruptionFilter();
+
+            Log.d("BroadCast: ", "currentmode " + currentMode);
+            Log.d("BroadCast: ", "case 1: " + NotificationManager.INTERRUPTION_FILTER_ALARMS);
+            Log.d("BroadCast: ", "case 2: " + NotificationManager.INTERRUPTION_FILTER_ALL);
+            Log.d("BroadCast: ", "case 3: " + NotificationManager.INTERRUPTION_FILTER_NONE);
+            Log.d("BroadCast: ", "case 4: " + NotificationManager.INTERRUPTION_FILTER_PRIORITY);
+            Log.d("BroadCast: ", "case 5: " + NotificationManager.INTERRUPTION_FILTER_UNKNOWN);
+
+            switch (currentMode){
+                case NotificationManager.INTERRUPTION_FILTER_ALL: {
+                    notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE);
+                    break;
+                }
+                case NotificationManager.INTERRUPTION_FILTER_UNKNOWN: {
+                    notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+                    handleRingVolume();
+                    break;
+                }
+                case NotificationManager.INTERRUPTION_FILTER_ALARMS: {
+                    notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+                    handleRingVolume();
+                    break;
+                }
+                case NotificationManager.INTERRUPTION_FILTER_NONE: {
+                    notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+                    handleRingVolume();
+                    break;
+                }
+                case NotificationManager.INTERRUPTION_FILTER_PRIORITY: {
+                    notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+                    handleRingVolume();
+                    break;
+                }
+            }
+
+
         }
     }
 }
--- app/src/main/java/com/junaidaziz/keepitsilent/MainActivity.java
+++ app/src/main/java/com/junaidaziz/keepitsilent/MainActivity.java
@@ -1,6 +1,7 @@
 package com.junaidaziz.keepitsilent;
 
 import android.Manifest;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
@@ -12,6 +13,7 @@ import android.content.*;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.net.Uri;
 import android.media.AudioManager;
@@ -19,21 +21,29 @@ import android.media.AudioManager;
 public class MainActivity extends AppCompatActivity {
 
     Context context;
+    NotificationManager notificationManager;
     private static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 101;
-    boolean audioPermission;
-    boolean readPhonePermission;
+    static boolean audioPermission =false;
+    static boolean readPhonePermission =false;
     static Button soundBtn;
+    static Button readCalls;
+    static Button audio;
+    static Button notificationsBtn;
+    static TextView btn_textView;
+    static TextView step_1_textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.context = this;
+        Log.d("Activity: ", "onCreate()");
 
-        audioPermission = checkForAudioPermission();
-        readPhonePermission = checkForReadPhonePermission();
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        Log.d("Permissions: ", audioPermission + " " + readPhonePermission);
+//        audioPermission = checkForAudioPermission();
+//        readPhonePermission = checkForReadPhonePermission();
 
 //        Intent incomingCall = new Intent(this.context, IncomingCall.class);
 //
@@ -45,13 +55,11 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
         soundBtn = findViewById(R.id.soundBtn);
-        soundBtn.setEnabled(false);
-        soundBtn.setOnClickListener(onSoundBtnListener);
-
-        if (!audioPermission){
-            audioPermission = checkForAudioPermission();
-        }
-
+        readCalls = findViewById(R.id.read_calls_permission);
+        audio = findViewById(R.id.audio_permission_Btn);
+        btn_textView = findViewById(R.id.btn_text);
+        step_1_textView = findViewById(R.id.step_1_textview);
+        notificationsBtn = findViewById(R.id.notification_Btn);
 
 
     }
@@ -60,23 +68,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        Log.d("Activity: ", "onResume()");
 
-        if (!readPhonePermission){
-            readPhonePermission = checkForReadPhonePermission();
+        readCalls.setEnabled(false);
+        readCalls.setText(R.string.permission_granted);
+        audio.setEnabled(false);
+        audio.setText(R.string.permission_granted);
+        soundBtn.setEnabled(false);
+        notificationsBtn.setEnabled(false);
+        notificationsBtn.setText(R.string.permission_granted);
+
+
+        if (!checkForAudioPermission()){
+            audio.setEnabled(true);
+            audio.setText(R.string.audio);
+            audio.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    askForAudioPermission(context);
+                }
+            });
         }
 
-        if (audioPermission && readPhonePermission){
+        if (!checkForReadPhonePermission()){
+            readCalls.setEnabled(true);
+            readCalls.setText(R.string.read_calls);
+            readCalls.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    askForPhonePermission();
+                }
+            });
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (!checkForNotificationsPermission()){
+                notificationsBtn.setEnabled(true);
+                notificationsBtn.setText(R.string.notifications);
+                notificationsBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        askForNotificationsPermission();
+                    }
+                });
+                Toast.makeText(context, "Notification Access Asked", Toast.LENGTH_LONG).show();
+            }  else {
+                Toast.makeText(context, "Notification Access Granted", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if (checkForPermissions()){
             soundBtn.setEnabled(true);
-
+            soundBtn.setOnClickListener(onSoundBtnListener);
+            btn_textView.setVisibility(View.INVISIBLE);
+            step_1_textView.setText(R.string.step_1_done);
         }
-
     }
 
     Button.OnClickListener onSoundBtnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
-            if (checkForPermissions()) {
                 try {
                     //toggleSoundMode();
                     Intent intent = new Intent();
@@ -86,40 +137,41 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception e) {
                     Log.d("toggle sound", "fail at button, reason:", e);
                 }
-            } else {
-                Toast.makeText(MainActivity.this, "Permission Not granted", Toast.LENGTH_LONG).show();
-            }
         }
     };
 
     public boolean checkForPermissions(){
-
-            if (audioPermission && readPhonePermission){
+            if (checkForAudioPermission() && checkForReadPhonePermission() && checkForNotificationsPermission()){
                 return true;
             }  else {
                 return false;
             }
-
-
     }
+
 
     public boolean checkForAudioPermission(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if (!Settings.System.canWrite(context)){
                 Log.d("Permission: ", "at API 23+ statement");
-                showPermissionDialog(context);
+               return false;
             } else if (Settings.System.canWrite(context)) {
-                Log.d("Permission: ", "Granted");
-                readPhonePermission = true;
+                Log.d("Permission: ", "Audio Granted");
                 return true;
             }
         } else if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_SETTINGS) == PackageManager.PERMISSION_GRANTED){
+            Log.d("Permission: ", "Audio Granted");
             return true;
         }
-
-
         return false;
     }
+
+    public void askForAudioPermission(final Context c){
+        Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+        intent.setData(Uri.parse("package:" + c.getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -128,6 +180,11 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     try {
                         readPhonePermission = true;
+
+                        if (checkForReadPhonePermission() && checkForAudioPermission()){
+                            soundBtn.setEnabled(true);
+                        }
+
                     } catch (Exception e) {
                         Log.d("Copying Database", "fail at menu location, reason:", e);
                     }
@@ -135,30 +192,47 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(context, "Permission Not granted", Toast.LENGTH_LONG).show();
                 }
             }
+            case Intent.FLAG_ACTIVITY_NEW_TASK: {
+
+            }
         }
     }
 
     public boolean checkForReadPhonePermission(){
 
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED){
-            Toast.makeText(context, "READ PHONE STATE PERMISSION GRANTED", Toast.LENGTH_LONG).show();
+//            Toast.makeText(context, "READ PHONE STATE PERMISSION GRANTED", Toast.LENGTH_LONG).show();
+            Log.d("Permission: ", "Read Phone Granted");
             return true;
         } else {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
             return false;
         }
 
     }
 
+    public void askForPhonePermission(){
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_PHONE_STATE},
+                MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+    }
 
-    public void showPermissionDialog(final Context c){
-        Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
-        intent.setData(Uri.parse("package:" + c.getPackageName()));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    public boolean checkForNotificationsPermission(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && notificationManager.isNotificationPolicyAccessGranted()) {
+            Log.d("Permission: ", "Notifications Granted");
+            return true;
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void askForNotificationsPermission(){
+        Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
         startActivity(intent);
     }
+
 
 
 
